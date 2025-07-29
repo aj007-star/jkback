4b475eb92969d10f4ff9f2f0bfd1e9ed1f4b801e
aj007-star <ajpowercoc2002@gmail.com>
2025-07-29 18:42:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require('path');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.options('*', cors());
 app.use(cors({
   origin: 'https://jkfront.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -22,6 +23,15 @@ app.use('/ProductImages', express.static(productImagesDir));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://jkfront.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204); // important: respond to preflight
+  }
+  next();
   res.status(500).json({ message: 'An error occurred!', error: err.message,success: false });
 });
 
