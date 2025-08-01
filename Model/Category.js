dff33a56479926dbab6f8857b8e7e095d788f816
aj007-star <ajpowercoc2002@gmail.com>
2025-08-01 16:06:26 +0530

    new cat

--- Model/Category.js
+++ Model/Category.js
@@ -1,12 +1,12 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../Config/dbConnect'); 
+const { sequelize } = require('../Config/dbConnect'); // ✅ Destructure sequelize correctly
+
 const Category = sequelize.define('Category', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
   }
-});   
-
+});
 
 Category.sync()
   .then(() => {
