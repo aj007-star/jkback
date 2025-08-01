f7767f45920cfcd7b64ccda1d3d3314ed1a5705c
aj007-star <ajpowercoc2002@gmail.com>
2025-08-01 17:43:58 +0530

    new all4567

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -2,7 +2,16 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('u189645309_jk', 'u189645309_jk', 'Shop@8899', {
   host: 'srv1645.hstgr.io',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  pool: {
+    max: 5,       // max connections
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  dialectOptions: {
+    connectTimeout: 10000
+  }
 });
 
 let isConnected = false;
